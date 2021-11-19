6747146127591f9147d13df5bc11a1039cf75281
Michael Chan <mijoch@gmail.com>
2021-11-19 10:14:42 -0800

    supabase setup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,20 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script>
-	import Counter from '$lib/Counter.svelte';
+    import {user} from "$lib/session-store"
+    import {supabase} from "$lib/supabase-client"
+    import Auth from "./Auth.svelte"
+    import Profile from "./Profile.svelte"
+
+    user.set(supabase.auth.user())
+
+    supabase.auth.onAuthStateChange((_, session) => {
+        user.set(session.user)
+    })
 </script>
 
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
-
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="container" style="padding: 50px 0 100px 0;">
+    {#if $user}
+        <Profile />
+    {:else}
+        <Auth />
+    {/if}
+</div>
